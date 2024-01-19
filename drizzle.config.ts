405e004992c0d8bb524704723e68c8011b1d1326
Ingo Valente <ingo@ingovalente.com>
2024-01-19 12:42:46 +0000

    One more time

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,10 @@
-import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
-
-console.log(
-  'Pushing?',
-  process.env.RENDER === 'true' ? '/var/data/sqlite.db' : './sqlite.db',
-);
 
 export default {
   schema: './src/server/schema.ts',
   driver: 'better-sqlite',
   out: './drizzle',
   dbCredentials: {
-    url: process.env.RENDER === 'true' ? '/var/data/sqlite.db' : './sqlite.db',
+    url: process.env.DB_URL || './sqlite.db',
   },
 } satisfies Config;
