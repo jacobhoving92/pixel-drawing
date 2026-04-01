07a90a0fd59e5dd66d483ab6bd7621484c80411e
Ingo Valente <ingo@ingovalente.com>
2026-04-01 14:58:11 +0200

    POST

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,9 +11,9 @@ import { Canvas } from './canvas';
 const PORT = process.env.PORT || 3000;
 const HEADERS = {
   'Content-Security-Policy':
-    "default-src 'self';base-uri 'self'; font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+    "default-src 'self';base-uri 'self'; font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self' 'unsafe-inline';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
   'Cross-Origin-Opener-Policy': 'same-origin',
-  'Cross-Origin-Resource-Policy': 'cross-origin',
+  'Cross-Origin-Resource-Policy': 'same-origin',
   'Origin-Agent-Cluster': '?1',
   'Referrer-Policy': 'no-referrer',
   'Strict-Transport-Security': 'max-age=15552000; includeSubDomains',
