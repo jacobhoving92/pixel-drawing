f7bebd350b4b07c82e52b768e83b01bffb4633b4
Ingo Valente <ingo@ingovalente.com>
2026-04-01 13:38:59 +0200

    CORS

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const HEADERS = {
   'Content-Security-Policy':
     "default-src 'self';base-uri 'self'; font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
   'Cross-Origin-Opener-Policy': 'same-origin',
-  'Cross-Origin-Resource-Policy': 'same-origin',
+  'Cross-Origin-Resource-Policy': 'cross-origin',
   'Origin-Agent-Cluster': '?1',
   'Referrer-Policy': 'no-referrer',
   'Strict-Transport-Security': 'max-age=15552000; includeSubDomains',
